b6d8cb9680d4698ad737946ba175b5221cdcbe78
khalidwebmail <csewebmail@gmail.com>
2022-07-30 17:53:27 +0600

    install route package

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+use Pecee\SimpleRouter\SimpleRouter;
+
+require_once 'routes/route.php';
+
+SimpleRouter::setDefaultNamespace('\Demo\Controllers');
+
+SimpleRouter::start();
